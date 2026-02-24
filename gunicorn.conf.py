84000ef8c2c178bcdf36973c3588a6826785a08a
phith0n <phith0n.ph2f@gmail.com>
2026-02-24 23:21:59 +0800

    Fix issue: refactor gunicorn configuration to use app_config for loading server settings

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,8 +4,8 @@ import yaml
 config_path = os.environ.get("CONFIG_PATH", "config.yaml")
 try:
     with open(config_path, "r", encoding="utf-8") as f:
-        config = yaml.safe_load(f)
-    server_conf = config.get("server", {})
+        app_config = yaml.safe_load(f)
+    server_conf = app_config.get("server", {})
     host = server_conf.get("host", "0.0.0.0")
     port = server_conf.get("port", 5000)
 except Exception:
